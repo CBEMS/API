529cee8312208a11d91d89fc6815ff0c2cf49aa0
Ahmed H. Moneeb <ahmedhamdy395@gmail.com>
2016-07-10 00:00:33 +0200

    edited the send message to send to device

--- message/send_message.php
+++ message/send_message.php
@@ -1,16 +1,28 @@
 <?PHP
 require("../DBConnection.php");
 
-$node_id = $_GET['node_id'];
+$device_id = $_GET['device_id'];
 $message = $_GET['message'];
+
+
+$sql = "SELECT devices.node_id
+FROM devices
+WHERE device_id = $device_id;";
+$result = $DB->query($sql);
+
+if ($result->num_rows > 0)
+{
+
+$row = $result->fetch_assoc();
+$node_id = $row['node_id'];
 
 $sql = "INSERT INTO messages (node_id, message, state)
 VALUES ('$node_id', '$message', 'not sent');";
-
 $result = $DB->query($sql);
 
 if ($result === TRUE) { echo "done"; }
 else { echo "wrong"; }
+}
 
 $DB->close();
 ?>
